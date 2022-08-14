bcf8ba1dfd0ee2b1a6a4cf6529ca3aa3072a929d
Dmitry <babanov91@gmail.com>
2022-08-14 19:33:24 +0500

    first commit

--- settings.py
+++ settings.py
@@ -6,12 +6,6 @@ load_dotenv()
 app = Flask('app')
 
 class app_settings():
-    # user = 'postgres'
-    # password = 'postgres'
-    # database = 'fl_adv3'
-    # user = 'app'
-    # password = '1234'
-    # database = 'fl_adv'
     db_name = os.environ.get('POSTGRES_DB')
     db_pass = os.environ.get('POSTGRES_PASSWORD')
     db_user = os.environ.get('POSTGRES_USER')
@@ -21,7 +15,6 @@ class app_settings():
 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
-    # app.config["SECRET_KEY"] = "sdf6as5df67sad54sa"
     app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
 app.config.from_object(app_settings)
